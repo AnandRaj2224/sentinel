3e2b0425fdf3efcdf5349fc057667fab8f3c65a0
Anand Raj <anandraj6783@gmail.com>
2026-06-27 21:56:47 +0530

    feat: add RateLimiter struct and constructor

--- main.go
+++ main.go
@@ -8,11 +8,30 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/joho/godotenv"
 )
 
+// RateLimiter struct contains visitors
+// that maps ip address to requests and
+// the frequency / no of time in a time period.
+type RateLimiter struct {
+	visitors map[string][]time.Time
+	mu       sync.RWMutex
+}
+
+// NewRateLimiter initalises a new RateLimiter struct
+// and return it address.
+func NewRateLimiter() *RateLimiter {
+	return &RateLimiter{
+		visitors: make(map[string][]time.Time),
+	}
+}
+
+// logging function that logs HTTP method,path,and duration to complete the request
+// in json format using slog.
 func LoggingMiddleware(logger *slog.Logger, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
@@ -29,33 +48,44 @@ func LoggingMiddleware(logger *slog.Logger, next http.Handler) http.Handler {
 }
 
 func main() {
+
 	// load the env so they are avalible to use.
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("error loading env!")
 	}
+
 	// port for the running server -> current proxy.
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
 	// target URL for the protected server.
 	targetURL := os.Getenv("TARGET_URL")
 	if targetURL == "" {
 		targetURL = "http://localhost:9000"
 	}
+
 	// parsing the main severs URL.
 	parsedURL, err := url.Parse(targetURL)
 	if err != nil {
 		log.Fatal("failed to parse the url!", err)
 	}
 
+	// creating a new slog object
+	// review
 	jsonHandler := slog.NewJSONHandler(os.Stdout, nil)
 
+	// review
 	logger := slog.New(jsonHandler)
+
+	// initialising the Rate Limiter.
+	limiter := NewRateLimiter()
 
 	// creating a new reverse Proxy with with main servers parsed URL.
 	proxy := httputil.NewSingleHostReverseProxy(parsedURL)
+
 	// logging contain the function + closure object returned from loggingMiddleware.
 	logging := LoggingMiddleware(logger, proxy)
 
