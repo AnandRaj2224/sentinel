7d26c0602aac8cefe8bad3404a32b150c334acb6
Anand Raj <anandraj6783@gmail.com>
2026-07-03 20:42:39 +0530

    feat: implement ratelimit middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -13,6 +14,23 @@ import (
 
 	"github.com/joho/godotenv"
 )
+// RateLimitMiddleware parses the IP address and checks it against the rate limit,if rate limit exceded
+// return a http.StatusTooManyRequests.
+func RateLimitMiddleware(limiter *RateLimiter, logger *slog.Logger, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			logger.Error("failed while parsing IP", err)
+		}
+		if limiter.Allow(ip) {
+			next.ServeHTTP(w, r)
+			return
+		}
+		logger.Warn("rate limit was exceeded, IP:", ip)
+		http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+		return
+	})
+}
 
 // responseRecorder wraps htt.responseWriter
 // to capture status code and body of downstream HTTP response.
