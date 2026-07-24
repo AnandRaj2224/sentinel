f7b6ba2094458f995de1e3dcc9c43bf8b3176bbd
Anand Raj <anandraj6783@gmail.com>
2026-07-24 20:29:26 +0530

    feat: add protected dummy server

---
+++ cmd/protectedServer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+func main() {
+
+	ports := []string{"9001", "9002"}
+	var wg sync.WaitGroup
+
+	for _, port := range ports {
+		wg.Add(1)
+		go func(port string) {
+			defer wg.Done()
+			err := http.ListenAndServe("localhost:"+port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				msg := fmt.Sprintf(`{"message": "Hello from Target Server %s"}`, port)
+				w.Write([]byte(msg))
+			}))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}(port)
+
+	}
+	wg.Wait()
+}
