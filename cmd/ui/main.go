291d6c63d8ae04bfeaeaac5f3e87543ceb241dcb
Anand Raj <anandraj6783@gmail.com>
2026-07-28 19:47:58 +0530

    feat: embed html dasboard file in UI server to have only one binary executable

--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"embed"
 	"log"
 	"net/http"
 )
 
-func main() {
+//go:embed index.html
+var files embed.FS
 
-	err := http.ListenAndServe("localhost:8081", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	}))
+func main() {
+	page, err := files.ReadFile("index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write(page)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
